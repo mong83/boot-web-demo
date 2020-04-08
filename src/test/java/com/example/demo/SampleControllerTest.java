e8cc3c433e0967d3e4ce786d7e06b0fe62a6bced
mong83 <06007sk@gmail.com>
2020-04-08 18:15:36 +0900

    HTTP 메세지컨버터 -기본

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -60,4 +60,13 @@ public class SampleControllerTest {
                 .andExpect(header().exists(HttpHeaders.CACHE_CONTROL))
         ;
     }
+
+    @Test
+    public void StringMessage() throws Exception {
+        this.mockMvc.perform(get("/message")
+                .content("hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
 }
