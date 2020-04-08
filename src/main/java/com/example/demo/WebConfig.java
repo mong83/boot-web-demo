66742ad4d91f983631e5c6a2cc57c61a0472893e
mong83 <06007sk@gmail.com>
2020-04-08 15:52:18 +0900

    HandlerInterceptor 구현

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(-1);
+    }
 }
